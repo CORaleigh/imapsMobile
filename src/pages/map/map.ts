c2c37d7eaf3b182990d3008d79f4f71c2ba83594
justingreco <justin.greco@raleighnc.gov>
2017-08-04 23:52:51 -0400

    zoom to property on map, hold on property on map to view info

--- src/pages/map/map.ts
+++ src/pages/map/map.ts
@@ -5,7 +5,9 @@ import {
   ViewChild
 } from '@angular/core';
 import {
-  NavController
+  NavController,
+  Events,
+  NavParams
 } from 'ionic-angular';
 import {
   EsriLoaderService
@@ -13,6 +15,10 @@ import {
 import {
   LayerListPage
 } from '../layer-list/layer-list';
+import {
+  PropertySearchProvider
+} from '../../providers/property-search/property-search';
+
 @Component({
   selector: 'page-map',
   templateUrl: 'map.html',
@@ -21,23 +27,137 @@ import {
 export class MapPage implements OnInit {
   @ViewChild('map') mapEl: ElementRef;
   map: any;
-  opLayers: Array<string> = [];
+  opLayers: Array < string > = [];
   layerList: any;
   loaded: boolean = false;
-  constructor(public navCtrl: NavController, private esriLoader: EsriLoaderService) {}
+  pin: string = null;
+  findParameters: any;
+  find: any;
+  query: any;
+  queryTask: any;
+  fillSymbol: any;
+  spatialReference: any;
+  agolPopupClickHandle: any;
+  agolPopupclickEventListener: any;
+  constructor(public navCtrl: NavController, public navParams: NavParams, private esriLoader: EsriLoaderService, private events: Events, private propertySearch: PropertySearchProvider) {
+    if (navParams.data.data) {
+      this.pin = navParams.data.data.account.pin;
+    }
+    events.subscribe('change-tab-info', (tab, data) => {
+      this.pin = navParams.data.data.account.pin;
+      if (this.map) {
+        this.findProperty(this.pin, this.findParameters, this.find, this.fillSymbol);
+      }
+    });
+  }
   goToLayerList() {
     this.navCtrl.push(LayerListPage, {
       map: this.map,
       opLayers: this.opLayers
     });
   }
+  findPropertyInfo(pin: string) {
+    this.propertySearch.getPropertyInfo(pin, 'pin').subscribe(results => {
+      let accounts = results.Accounts;
+      if (accounts.length > 1) {
+        this.events.publish('change-tab', 1, {
+          accounts: accounts,
+          fields: results.Fields
+        });
+      } else if (accounts.length === 1) {
+        this.events.publish('change-tab-info', 2, {
+          account: accounts[0],
+          fields: results.Fields
+        });
+      }
+    });
+  }
+  findProperty(pin: string, findParameters, findTask, fillSymbol) {
+    findParameters.layerIds = [0, 1];
+    findParameters.searchFields = ['PIN_NUM'];
+    findParameters.searchText = pin;
+    findParameters.returnGeometry = true;
+    findParameters.outSpatialReference = this.spatialReference;
+    console.log(this.spatialReference);
+
+    findTask.execute(findParameters).then(results => {
+      if (results.length > 0) {
+        results[0].feature.symbol = fillSymbol;
+        this.map.graphics.clear();
+        this.map.graphics.add(results[0].feature);
+        this.map.setExtent(results[0].feature.geometry.getExtent(), true);
+      }
+    });
+  }
+  findPropertyPin(point) {
+    this.queryTask.url = 'https://maps.raleighnc.gov/arcgis/rest/services/Parcels/MapServer/0';
+    this.query.geometry = point;
+    this.query.outSpatialReference = this.spatialReference;
+    this.query.outFields = ['PIN_NUM'];
+    this.query.where = '1=1';
+    this.queryTask.execute(this.query).then(response => {
+      if (response.features.length > 0) {
+        this.findPropertyInfo(response.features[0].attributes.PIN_NUM);
+      } else {
+        this.queryTask.url = 'https://maps.raleighnc.gov/arcgis/rest/services/Parcels/MapServer/1';
+        this.queryTask.execute(this.query).then(response => {
+          if (response.features.length > 0) {
+            this.findPropertyInfo(response.features[0].attributes.PIN_NUM);
+          }
+        })
+      }
+    }, error => {});
+
+  }
+
   ngOnInit() {
     this.esriLoader.load({
       url: 'https://js.arcgis.com/3.21/'
     }).then(() => {
-      this.esriLoader.loadModules(['esri/map', 'esri/arcgis/utils', 'esri/geometry/Point', 'esri/dijit/LayerList']).then(([Map, arcgisUtils, Point, LayerList]) => {
-        var page = this;
+      this.esriLoader.loadModules([
+        'esri/map',
+        'esri/arcgis/utils',
+        'esri/geometry/Point',
+        'esri/dijit/LayerList',
+        'esri/tasks/query',
+        'esri/tasks/QueryTask',
+        'esri/tasks/FindTask',
+        'esri/tasks/FindParameters',
+        'esri/symbols/SimpleFillSymbol',
+        'esri/SpatialReference'
+      ]).then(([Map, arcgisUtils, Point, LayerList, Query, QueryTask, FindTask, FindParameters, SimpleFillSymbol, SpatialReference]) => {
+        let page = this;
+        this.spatialReference = new SpatialReference(3857);
+        this.findParameters = new FindParameters();
+        this.find = new FindTask('https://maps.raleighnc.gov/arcgis/rest/services/Parcels/MapServer');
+        this.fillSymbol = new SimpleFillSymbol({
+          "color": [
+            255,
+            255,
+            255,
+            0
+          ],
+          "outline": {
+            "color": [
+              255,
+              0,
+              0,
+              255
+            ],
+            "width": 2.25,
+            "type": "esriSLS",
+            "style": "esriSLSSolid",
+            "marker": null
+          },
+          "type": "esriSFS",
+          "style": "esriSFSSolid"
+        });
+        this.query = new Query();
+        this.queryTask = new QueryTask('https://maps.raleighnc.gov/arcgis/rest/services/Parcels/MapServer/0');
         this.layerList = LayerList;
+        if (this.pin) {
+          this.findProperty(this.pin, this.findParameters, this.find, this.fillSymbol);
+        }
         arcgisUtils.createMap("dc14a00c3e4d474c9338f6b1e03234cd", this.mapEl.nativeElement, {
           mapOptions: {
             slider: false
@@ -48,14 +168,27 @@ export class MapPage implements OnInit {
             page.opLayers.push(opLayer.id);
           });
           page.map = response.map;
+          page.agolPopupClickHandle = response.clickEventHandle;
+          page.agolPopupclickEventListener = response.clickEventListener;          
+          if (page.pin) {
+            page.findProperty(page.pin, page.findParameters, page.find, page.fillSymbol);
+          }
           page.loaded = true;
           let timeout = null;
           page.map.on('mouse-down', (evt) => {
+                //connect editor
+          if (page.agolPopupClickHandle) {
+              page.agolPopupClickHandle.remove();
+              page.agolPopupClickHandle = null;
+          }
             timeout = setTimeout(() => {
-              console.log(evt);
+              page.findPropertyPin(evt.mapPoint);
             }, 1000);
           });
           page.map.on('mouse-up', (evt) => {
+            if (!page.agolPopupClickHandle) {
+                page.agolPopupClickHandle = page.map.on("click", page.agolPopupclickEventListener);
+            }
             if (timeout) {
               clearTimeout(timeout);
             }
@@ -65,3 +198,8 @@ export class MapPage implements OnInit {
     });
   }
 }
+
+
+
+
+
